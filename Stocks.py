bb49af4a5499095ab2feb61a0403d8d9755779a4
William13-byte <91633139+William13-byte@users.noreply.github.com>
2022-04-01 17:29:13 +0200

    fixat

---
+++ Stocks.py
@@ -0,0 +1,69 @@
+
+from bs4 import BeautifulSoup
+from time import sleep
+import requests
+import string 
+
+def main(företag):
+
+    class URL:
+        def __init__(self, stock,price = 0):
+            self.stock = stock
+            self.price = price
+        def pris(self):
+            url = f"https://finance.yahoo.com/quote/{self.stock}?p={self.stock}&.tsrc=fin-srch"
+            page = requests.get(url, headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+            })
+            soup = BeautifulSoup(page.text, 'html.parser')
+            self.price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
+            return self.price
+
+
+    def update(företag):
+        for värde in företag:
+            aktie = URL(värde)
+            företag[värde] = aktie.pris()
+            #print(i)
+        return företag
+
+    def text_to_number(sträng):
+        for index in range(len(sträng)):
+            if type(sträng[index]) == str:
+                isolerad = sträng[index]
+                if isolerad.find(","):
+                    lista_utan_comma = isolerad.split(",")
+                    #print (temp)
+                    isolerad = "".join(lista_utan_comma)
+                
+                sträng[index] = (float(isolerad))
+                
+        return sträng
+
+
+    def main(företag):
+        lista = []
+        index = 0
+       
+        företag = update(företag)
+        for värde in företag:
+            lista.append(företag[värde])
+            lista = text_to_number(lista)
+            företag[värde] = lista[index]
+            index+=1 
+        return företag
+        
+    företag = main(företag)
+    lista = []
+    for värde in företag.values():
+        #print (värde,1 )
+        lista.append(värde)
+    #print(lista)
+    return lista
+    
+#simulation
+"""
+test = {"AMZN": 0, "TSLA":0}
+for loop in range (5):
+    print(main(test))
+"""
---
+++ UI.py
@@ -0,0 +1,163 @@
+from tkinter import *
+import Stocks
+
+win = Tk()
+win.geometry("700x350")
+win.title("Aktier")
+y, antal_entries, antal_text, y_värdetext = 0,0,0,0
+
+
+namn = []
+företag1 = {}
+kör = False
+företag = []
+gammalt_v = []
+plotAktie = {}
+nyaAktier = 0
+
+def skapa_aktier():
+    global namn,  företag1, plotAktie
+    lista_namn = []
+    
+    for loop in namn:
+        företag1[loop] = 0
+        lista_namn.append(loop)
+        plotAktie[loop] = []
+    return lista_namn, företag1
+
+#kreaktion av aktier 
+def kreaktion():
+    global lista_namn, företag1, gammalt_v
+    lista_namn, företag1 = skapa_aktier()
+    for loop in range (len(företag1)):
+            gammalt_v.append(0)
+    temp = []
+    
+    
+    #print (gammalt_v)
+#stänga av programmet 
+def stäng():
+    global kör, hoppa
+    hoppa = False
+    kör = False
+
+    #print(plotAktie)
+        
+def print_text():
+   global företag
+  
+   if kör:
+        #själva programmet
+        #print (företag1)
+        for värde in range (len(företag1)):
+            företag = Stocks.main(företag1)
+        #    print (företag, 1 )
+     
+     #-------------------------------------
+        #Jämförelse av priserna, via det gammla priset mot det nu varande
+        #print (företag)
+        #print (gammalt_v, "gammalt")
+        for värde_f in range (len(namn)):
+            """
+            print (värde_f, "for, index ")
+            print (företag[värde_f], "företag")
+            print (gammalt_v[värde_f], "gammalt")
+            print(text_rutor[värde_f], "text_rutor")
+            """
+            if företag[värde_f] > gammalt_v[värde_f]:
+                text_rutor[värde_f].configure (text=f": {företag[värde_f]}$ ↑ +{round(företag[värde_f] - gammalt_v[värde_f],2 )}", fg= "green")
+                
+            
+            elif företag[värde_f] < gammalt_v[värde_f]:
+                text_rutor[värde_f].configure (text=f": {företag[värde_f]}$ ↓ -{round(gammalt_v[värde_f] - företag[värde_f], 2)}", fg= "red")
+                
+            else:
+                text_rutor[värde_f].configure(text=f": {företag[värde_f]}$ 〃{round(gammalt_v[värde_f]- företag[värde_f], 2)}", fg="gray")
+            gammalt_v.pop(värde_f)
+            gammalt_v.insert(värde_f, företag[värde_f])
+           
+
+   win.after(1000, print_text)
+
+
+def på_start():
+   global kör
+   kör = True
+  
+
+def på_stopp():
+   global kör
+   kör = False
+
+def konfimermation():
+    global namn, antal_entries, nyaAktier
+  
+    for i in range(antal_entries):
+        namn.append(skriv_rutor[i].get().upper())
+    
+    #    print(namn)
+        skapa_aktier()
+        kreaktion()
+        nyaAktier +=1
+    namn = list(dict.fromkeys(namn))
+    #print(namn)
+
+skriv_rutor = []
+def lägg_till():
+    global y, antal_entries
+    
+    for x in range(1):
+        if antal_entries <= 3:
+            skriv_ruta = Entry(win)
+            skriv_ruta.grid(row=y, column=0, pady=5, padx=5)
+            skriv_rutor.append(skriv_ruta)
+            antal_entries+=1 
+            y+=1
+    lägg_till_text()
+
+text_rutor =[]
+def lägg_till_text():
+    global antal_text, y_värdetext
+    if antal_text <= 3:
+       text_ruta = Label(win)
+       text_ruta.grid(row=y_värdetext, column=1, padx = 5, pady=5)
+       text_rutor.append(text_ruta)
+       antal_text+=1
+       y_värdetext +=1 
+
+def tabort():
+    global antal_text, antal_entries, skriv_rutor, företag1, gammalt_v, företag, lista_namn
+    if len(företag) >= 1:
+        företag1.popitem()
+        gammalt_v.pop(-1)
+        företag.pop(-1)
+        lista_namn.pop(-1)
+        namn.pop(-1)
+        plotAktie.popitem()
+    
+    text_rutor[-1].destroy()
+    text_rutor.pop(-1)
+    antal_entries-=1
+    skriv_rutor[-1].destroy()
+    skriv_rutor.pop(-1)
+    antal_text-=1 
+    
+x_värde = 210
+start = Button(win, text="Start", command=på_start)
+start.place(x=x_värde, y=150, anchor="center")
+
+stopp_knapp = Button(win, text="Stop", command=på_stopp)
+stopp_knapp.place(x=x_värde+40, y=150, anchor="center")
+
+add_knapp = Button(win, text="Lägg till", command = lägg_till)
+add_knapp.place(x=x_värde, y=190, anchor="center")
+
+delete_knapp = Button(win, text="Ta bort", command=tabort)
+delete_knapp.place(x = x_värde + 60, y=190, anchor="center")
+
+confirm = Button(win, text= "Confirm", command=konfimermation)
+confirm.place(x=x_värde, y=230, anchor="center")
+
+win.after(1000, print_text)
+
+win.mainloop()
